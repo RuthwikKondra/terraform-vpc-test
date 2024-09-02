28b4c1bf4b3ae5c77998e948ec9ee55ed53ec1b8
Kondra Ruthwik <ruthwikkondra13@gmail.com>
2024-09-02 20:30:05 +0530

    added files

---
+++ variables.tf
@@ -0,0 +1,35 @@
+variable "project_name" {
+    default = "expense"
+  
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Enviornment = "dev"
+        Terraform = "true"
+    }
+  
+ }
+
+variable "public_subnet_cidrs" {
+     default = ["10.0.1.0/24" ,"10.0.2.0/24"]
+
+  
+}
+
+variable "private_subnet_cidrs" {
+     default = ["10.0.11.0/24" ,"10.0.12.0/24"]
+
+  
+}
+
+variable "dataBase_subnet_cidrs" {
+     default = ["10.0.21.0/24" ,"10.0.22.0/24"]
+
+  
+}
+
+variable "is_peering_required" {
+  default = true
+}
